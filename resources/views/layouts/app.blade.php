3eb2bdb968bbb053c6e8a447f3bfdc8028177cfe
Marin <marin.xile@gmail.com>
2024-08-03 16:47:35 +0900

    Revert "test marin"
    This reverts commit 36e6c2858cd563f2ffb9b5ddcc7d5df9ccc9e38b.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
-
+           
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
@@ -37,4 +37,3 @@
 
 <!-- github test -->
 <!-- misa test -->
-{{-- marin test --}}
