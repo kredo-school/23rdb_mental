6d129268953a367505978c4bdd2267260b40658d
Marin <marin.xile@gmail.com>
2024-08-08 16:20:45 +0900

    [UI/register/marin] - Login/Register UI
    added some images to public/images file
    completed UI login/register pages
    added logo and logout link to nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>{{ config('app.name') }} | @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     {{-- CSS Style Sheet --}}
@@ -23,24 +23,38 @@
     <!-- Include any other CSS or scripts needed -->
 </head>
 <body>
-    <header>
-        <nav>
-            <!-- Navigation links -->
-            
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
+    <div id="app">
+        <nav class="navbar navbar-expand-md shadow">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('images/main/logo_sm.png') }}" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                <i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
-    </header>
-    <main>
-        @yield('content')
-    </main>
-    <footer>
-        <!-- Footer content -->
-        &copy; {{ date('Y') }} MEntal
-    </footer>
-    <!-- Include any scripts needed -->
+
+        <main class="py-5">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
