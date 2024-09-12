9fee1c9404d435ed8fd10964282b7c612a2bd708
Misa Fujita <misafujico@gmail.com>
2024-09-12 23:54:48 +0900

    UI/navbar/misa Fixed Navbar, Footer, Sidebar

--- resources/views/components/navbar-each.blade.php
+++ resources/views/components/navbar-each.blade.php
@@ -1,9 +1,6 @@
 <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
-{{-- @extends('layouts.app') --}}
-{{-- @section('title', 'each') --}}
-{{-- @section('content') --}}
 
-<nav class="navbar sticky-top navbar-dark navbar-expand-md shadow">
+<nav class="navbar navbar-dark navbar-expand-md shadow sticky-top">
 
     <div class="container-fluid">
         <!-- Left Side Of Navbar -->
@@ -13,30 +10,30 @@
                 </a>
                 <p class="nav_pageTitle">@yield('title')</p>
             </div>
+        
         <!-- Toggler button for narrow view -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        
+        
         <!-- Right Side Of Navbar -->
-        <div class="ms-auto">
-            <div class="collapse navbar-collapse">
+        <div class="d-flex ms-auto align-items-center">
+            <a href="{{ route('profile.show', Auth::user()->id) }}">
+                @if (Auth::check() && Auth::user()->avatar)
+                    <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" class="nav_avatar fa-solid fa-circle-user"></a>
+                @else
+                    <i class="nav_avatar fa-solid fa-circle-user"></i>
+                @endif
 
-                <a href="{{ route('profile.show', Auth::user()->id) }}" class="sidebar_avatar">
-                    @if (Auth::check() && Auth::user()->avatar)
-                        <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" class="sidebar_avatar fa-solid fa-circle-user"></a>
-                    @else
-                        <i class="nav_avatar fa-solid fa-circle-user"></i>
-                    @endif
-                
-                <a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                    <button class="btn-logout">Logout</button>
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
+            <a href="{{ route('login') }}"
+                onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                <button class="btn-logout">Logout</button>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </div>
     </div>
 </nav>
--- resources/views/components/sidebar-admin.blade.php
+++ resources/views/components/sidebar-admin.blade.php
@@ -1,74 +1,59 @@
-<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
-{{-- @extends('layouts.app') --}}
-{{-- @section('title', 'sidebar-admin') --}}
-{{-- @section('content') --}}
-    <!-- Main Menu -->
+<link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+
     <div class="body_sidebar">
+        <!-- Main Menu -->
         <nav class="sidebar_inner">
-            <p class=" sidebar_h1">ENCOURAGE YOURSELF, <br>HEAL YOURSELF</p>
-            <br>
-            <div class="sidebar a">
-                <a href="{{ route('home') }}">
+
+            <p class=" sidebar_title">ENCOURAGE YOURSELF, <br>HEAL YOURSELF</p>
+            
+            <div class="sidebar">
+                <a href="{{ route('home') }}" class="sidebar_icon">
                     <p class="sidebar_icon"><img src="{{ asset('images/navigation/home_side_icon.png') }}" alt="home">Home</p>
                 </a>
-                <a href="{{ route('mood.index') }}">
-                    <p class="sidebar_icon"><img src="{{ asset('images/navigation/mood_side_icon.png') }}" alt="MoodTracking">Mood_Tracking</p>
+                <a href="{{ route('mood.index') }}" class="sidebar_icon">
+                    <p class="sidebar_icon @yield('title')_mood"><img src="{{ asset('images/navigation/mood_side_icon.png') }}" alt="MoodTracking">Mood Tracking                    
+                    </p>
                 </a>
-                <a href="{{ route('journal.journals') }}">
-                    <p class="sidebar_icon"><img src="{{ asset('images/navigation/journaling_side_icon.png') }}" alt="Journaling">Journal</p>
+                <a href="{{ route('journal.journals') }}" class="sidebar_icon">
+                    <p class="sidebar_icon @yield('title')_journal"><img src="{{ asset('images/navigation/journaling_side_icon.png') }}" alt="Journal">Journal</p>
                 </a>
-                <a href="{{ route('quote.index') }}">
-                    <p class="sidebar_icon"><img src="{{ asset('images/navigation/quote_side_icon.png') }}" alt="Quote">Quote</p>
-                </a>
-                <a href="{{ route('chatroom.index') }}">
-                    <p class="sidebar_icon"><img src="{{ asset('images/navigation/chat_side_icon.png') }}" alt="Chat">Chat</p>
+                {{-- <a href="{{ route('quote.index') }}" class="sidebar_icon">
+                    <p class="sidebar_icon @yield('title')_quote"><img src="{{ asset('images/navigation/quote_side_icon.png') }}" alt="Quote">Quote</p>
+                </a> --}}
+                <a href="{{ route('chat.chats', '1') }}" class="sidebar_icon">
+                    <p class="sidebar_icon @yield('title')chat"><img src="{{ asset('images/navigation/chat_side_icon.png') }}" alt="Chat">Chat</p>
                 </a>
             </div>
-            <br>
+        </nav>
+        
         <!-- Admin Menu -->
-            <div class=".ls_sidebar_admin a">
-                <p class="sidebar_icon_admin"><img src="{{ asset('images/navigation/admin_side_icon.png') }}" alt="home">Admin Menu</p>
+        <nav class="sidebar_inner">
+            <div class="ls_sidebar_admin">
+                <img src="{{ asset('images/navigation/admin_side_icon.png') }}" alt="home">Admin Menu
                 <ul>
                     <li class="ls_sidebar_admin"><a href="{{ route('users.index') }}">All Users</a></li>
                     <li class="ls_sidebar_admin"><a href="{{ route('quote.index') }}">All Quotes</a></li>
                     <li class="ls_sidebar_admin"><a href="{{ route('admin.chats.index') }}">All Chat</a></li>
                     <li class="ls_sidebar_admin"><a href="{{ route('admin.inquiries') }}">All Inquiry</a></li>
-                    <hr class="sidebar_hr1">
                 </ui>
             </div>
         </nav>
 
-        </nav>
         <!-- Footer Menu -->
-        <nav class="sidebar_inner_bottom">
-            <div class="sidebar a">
-                <a href="{{ route('profile.show', Auth::user()->id) }}" class="nav_avatar_link"></a>
-                    @if (Auth::check() && Auth::user()->avatar)
-                        <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" class="nav_avatar fa-solid fa-circle-user">
-                    @else
-                        <i class="nav_avatar fa-solid fa-circle-user"></i>
-                    @endif
-                <a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                    <p class="sidebar_icon align-items-center"><img src="{{ asset('images/navigation/logout_side_icon.png') }}" alt="Logout">Logout</p>
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-                <p></p>
-                    <div class="modal-body">
-                        <a class="sidebar_contact" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</a>
-                    </div>
+        <nav class="sidebar_inner_footer">
+            <div>
+                <a href="{{ url('/') }}" class="sidebar_faq"> F A Q </a>
+            </div>
+
+            <div class="modal-body">
+                <button class="sidebar_contact mt-3" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</button>
             </div>
         </nav>
     </div>
+
     @include('contactus.modals.inquiry')
     @include('contactus.modals.submitcomplete')
-{{-- @endsection --}}
-@section('scripts')
-    <script src="{{ asset('js/inquiry.js') }}"></script>
-@endsection
+
 
 
 
