72668817b1c1f8a5142726f149ac9d78d9056009
Misa Fujita <misafujico@gmail.com>
2024-09-16 14:28:48 +0900

    UI/navbar/misa I updated 2 sidebars.

--- resources/views/components/sidebar-admin.blade.php
+++ resources/views/components/sidebar-admin.blade.php
@@ -11,8 +11,7 @@
                     <p class="sidebar_icon"><img src="{{ asset('images/navigation/home_side_icon.png') }}" alt="home">Home</p>
                 </a>
                 <a href="{{ route('mood.index') }}" class="sidebar_icon">
-                    <p class="sidebar_icon @yield('title')_mood"><img src="{{ asset('images/navigation/mood_side_icon.png') }}" alt="MoodTracking">Mood Tracking                    
-                    </p>
+                    <p class="sidebar_icon @yield('title')_mood"><img src="{{ asset('images/navigation/mood_side_icon.png') }}" alt="MoodTracking">Mood Tracking</p>
                 </a>
                 <a href="{{ route('journal.journals') }}" class="sidebar_icon">
                     <p class="sidebar_icon @yield('title')_journal"><img src="{{ asset('images/navigation/journaling_side_icon.png') }}" alt="Journal">Journal</p>
@@ -23,37 +22,74 @@
                 <a href="{{ route('chat.chats', '1') }}" class="sidebar_icon">
                     <p class="sidebar_icon @yield('title')_chat"><img src="{{ asset('images/navigation/chat_side_icon.png') }}" alt="Chat">Chat</p>
                 </a>
+                <p class="fs-6 text-muted mb-1"><img src="{{ asset('images/navigation/admin_side_icon.png') }}" alt="home">Admin Menu</p>
             </div>
-        </nav>
+
         
         <!-- Admin Menu -->
-        <nav class="sidebar_inner">
             <div class="ls_sidebar_admin">
-                <img src="{{ asset('images/navigation/admin_side_icon.png') }}" alt="home">Admin Menu
-                <ul>
-                    <li class="ls_sidebar_admin"><a href="{{ route('users.index') }}">All Users</a></li>
-                    <li class="ls_sidebar_admin"><a href="{{ route('quote.index') }}">All Quotes</a></li>
-                    <li class="ls_sidebar_admin"><a href="{{ route('admin.chats.index') }}">All Chat</a></li>
-                    <li class="ls_sidebar_admin"><a href="{{ route('admin.inquiries') }}">All Inquiry</a></li>
-                    <li class="ls_sidebar_admin"><a href="{{ route('admin.inquiries') }}">All deletion reasons</a></li>
+                <ul class="ls_sidebar_item">
+                    <li>
+                        <a href="{{ route('users.index') }}" id="users_active" class="ls_sidebar_item">All Users</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('quote.index') }}" id="quote_active" class="ls_sidebar_item">All Quotes</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.chats.index') }}" id="chat_active" class="ls_sidebar_item">All Chat</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.inquiries') }}" id="inquiry_active" class="ls_sidebar_item">All Inquiry</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('deletion-reasons.index') }}" id="deletion_active" class="ls_sidebar_item">All Deletion reasons</a>
+                    </li>
                 </ui>
             </div>
         </nav>
 
+
         <!-- Footer Menu -->
         <nav class="sidebar_inner_footer">
             <div>
-                <a href="{{ url('/') }}" class="sidebar_faq"> F A Q </a>
+                <a href="{{ url('faq') }}" class="sidebar_faq"> FAQ </a>
             </div>
 
             <div class="modal-body">
-                <button class="sidebar_contact mt-3" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</button>
+                <a class="sidebar_contact mt-3" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</a>
             </div>
         </nav>
     </div>
 
     @include('contactus.modals.inquiry')
     @include('contactus.modals.submitcomplete')
+
+
+
+
+    {{-- <li>
+                        <a href="{{ route('users.index') }}" id="@yield('title')_users" class="ls_sidebar_item">All Users</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('quote.index') }}" class="ls_sidebar_item">
+                            <p class="ls_sidebar_item" id="@yield('title')_users">All Quotes</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.chats.index') }}" class="ls_sidebar_item">
+                            <p class="ls_sidebar_item" id="@yield('title')_users">All Chat</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.inquiries') }}" class="ls_sidebar_item">
+                            <p class="ls_sidebar_item" id="@yield('title')_users">All Inquiry</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('deletion-reasons.index') }}" class="ls_sidebar_item">
+                            <p class="ls_sidebar_item" id="@yield('title')_users">All Deletion Reasons</p>
+                        </a>
+                    </li> --}}
 
 
 
