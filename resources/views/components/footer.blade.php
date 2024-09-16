e1dea2d3a0be406e27830cbb20193caa09e66cf9
Misa Fujita <misafujico@gmail.com>
2024-09-16 09:23:45 +0900

    UI/navbar/misa I finalized footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,20 @@
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 
-<footer class="fixed-bottom">
+{{-- <footer>
         <div class="d-md-flex justify-content-md-end align-items-center p-3">
                 <p class="copyright me-4 mt-3">MEntal copyright 2024 Designed and built by Kred 23rd Batch B</p>
                 <a href="#" class="faq me-4">FAQ</a>
                 <button class="btn-contact me-2" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</button>
         </div>    
+</footer> --}}
+
+<footer class="footer d-flex justify-content-md-end align-items-center p-3">
+    <div class="footer_container">
+      <a href="#" class="faq me-4">FAQ</a>
+      <button class="btn-contact me-2" data-bs-toggle="modal" data-bs-target="#ContactUs">Contact Us</button>
+    </div>
 </footer>
 
     @include('contactus.modals.inquiry')
     @include('contactus.modals.submitcomplete')
 
-
-@section('scripts')
-    <script src="{{ asset('js/inquiry.js') }}"></script>
-@endsection
