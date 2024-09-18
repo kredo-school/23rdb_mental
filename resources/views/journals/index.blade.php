aa97f7f3a8a50f4e0449befb7d29c29b6e33807d
sachi-yama <hskwsck@yahoo.co.jp>
2024-09-18 20:45:28 +0900

    backend/chats
    - realtime chat
    - UI improve

--- resources/views/journals/index.blade.php
+++ resources/views/journals/index.blade.php
@@ -2,7 +2,7 @@
 
 @extends('layouts.app')
 @extends('components.navbar-each')
-@section('title', 'Journal')
+@section('title', 'Journaling')
 
 @section('content')
 @if(Auth::user()->role_id == 1)
@@ -137,7 +137,7 @@
                 </div>
             @empty
                 <div class="lead text-muted text-center">
-                    No Journal Found.
+                    No Journaling Found.
                 </div>
             @endforelse
         </div>
